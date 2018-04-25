8664d69e8d33612df73ef62040c7301383b01a91
Myron Voitovych <traftar4ik@gmail.com>
2018-04-25 12:39:44 +0300

    completed

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 			data: th.serialize()
 		}).done(function() {
 			alert("Спасибо! Ваш заказ принят на обработку!");
-            window.location.replace("http://localhost:63342/nicer-dicer/index.html?_ijt=mefe4lfv77agbthh0cnead5hv6");
+            window.location.replace("http://nicer-dicer/");
 			setTimeout(function() {
 				// Done Functions
 				th.trigger("reset");
